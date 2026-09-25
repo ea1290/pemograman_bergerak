3ec971d971374f604bc5640ca3db8a3d9b5470de
ea1290 <eaea07474@gmail.com>
2026-09-25 18:00:00 +0700

    25 SEPTEMBER

---
+++ kalkulator.dart
@@ -0,0 +1,18 @@
+class Kalkulator {
+  double number1;
+  double number2;
+
+  Kalkulator(this.number1, this.number2);
+
+  double add() => number1 + number2;
+  double subtract() => number1 - number2;
+
+  double multiply() => number1 * number2;
+
+  double divide() {
+    if (number2 == 0){
+      throw Exception('Tidak dapat membagi dengan nilai nol');
+    }
+    return number1 / number2;
+  }
+}
---
+++ main.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+import 'kalkulator.dart';
+
+void main(){
+
+  double readNumber(String message){
+    while (true){
+      stdout.write(message);
+
+      final input = stdin.readLineSync();
+
+      final number = double.tryParse(input ?? '');
+
+      if (number != null){
+        return number;
+      }
+
+      print('Input harus berupa angka');
+    }
+  }
+
+  void showMenu(){
+    print('\nPilih operasi:');
+    print('[1] Tambah');
+    print('[2] Kurang');
+    print('[3] Kali');
+    print('[4] Bagi');
+  }
+
+  int readChoice(){
+    while (true){
+      stdout.write('Masukkan pilihan (1-4): ');
+
+      final input = stdin.readLineSync();
+
+      final choice = int.tryParse(input ?? '');
+
+      if(choice != null && choice >= 1 && choice <= 4) {
+        return choice;
+      }
+
+      print('Pilihan harus berupa angka (1-4).');
+    }
+  }
+
+  void calculate(Kalkulator Kalkulator, int choice){
+    try{
+      final result = switch(choice){
+        1 => Kalkulator.add(),
+        2 => Kalkulator.subtract(),
+        3 => Kalkulator.multiply(),
+        4 => Kalkulator.divide(),
+        _ => throw StateError('Pilihan tidak valid.'),
+      };
+
+      print('\nHasil: $result');
+    } catch (e) {
+      print('\nError: $e');
+    }
+  }
+
+bool readAgain(){
+  stdout.write('\nApakah ingin melakukan perhitungan lagi? (Y/T): ');
+  final input = stdin.readLineSync()?.trim().toUpperCase();
+  return input == 'Y';
+  }
+}
